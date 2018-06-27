bca020027b86ce1c583869c71fe8a2558d9cc873
Denis Osipov <whomi@mail.ru>
2018-06-27 18:47:12 +0300

    initial commit

---
+++ text/popUp.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+
+function PopUpShow(){ //Функция отображения PopUp
+    $("#popup1").show();
+}
+
+function PopUpHide(){ //Функция скрытия PopUp
+    $("#popup1").hide();
+}
